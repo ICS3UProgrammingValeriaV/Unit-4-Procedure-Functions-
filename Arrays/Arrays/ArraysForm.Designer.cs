3a85d68b66b29ce00b7335162d5832bafbe080ae
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-11-29 10:50:26 -0500

    Change

---
+++ Arrays/Arrays/ArraysForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Arrays
+{
+    partial class frmArrays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstMarks = new System.Windows.Forms.ListBox();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lblAverage2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(140, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(80, 31);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstMarks
+            // 
+            this.lstMarks.FormattingEnabled = true;
+            this.lstMarks.Location = new System.Drawing.Point(107, 87);
+            this.lstMarks.Name = "lstMarks";
+            this.lstMarks.Size = new System.Drawing.Size(149, 147);
+            this.lstMarks.TabIndex = 1;
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAverage.Location = new System.Drawing.Point(107, 257);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(92, 31);
+            this.btnAverage.TabIndex = 2;
+            this.btnAverage.Text = "Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.ForeColor = System.Drawing.Color.White;
+            this.lblAverage.Location = new System.Drawing.Point(220, 259);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(0, 25);
+            this.lblAverage.TabIndex = 3;
+            // 
+            // lblAverage2
+            // 
+            this.lblAverage2.AutoSize = true;
+            this.lblAverage2.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(128)))), ((int)(((byte)(95)))));
+            this.lblAverage2.Location = new System.Drawing.Point(226, 263);
+            this.lblAverage2.Name = "lblAverage2";
+            this.lblAverage2.Size = new System.Drawing.Size(0, 25);
+            this.lblAverage2.TabIndex = 4;
+            // 
+            // frmArrays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(17)))));
+            this.ClientSize = new System.Drawing.Size(387, 318);
+            this.Controls.Add(this.lblAverage2);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.lstMarks);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmArrays";
+            this.Text = "Arrays by Valeria V";
+            this.Load += new System.EventHandler(this.frmArrays_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstMarks;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblAverage2;
+    }
+}
+
---
+++ Arrays/Arrays/ArraysForm.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Created by: Valeria Veverita
+ * Created on: 29-November-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #36 - Introduction to Arrays
+ * This program generates random numbers and calculate their average
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrays
+{
+    public partial class frmArrays : Form
+    {
+        //declare global variales and constants
+        const int MAX_SIZE = 10;
+        int[] arrayOfMarks = new int[MAX_SIZE];
+
+        public frmArrays()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            Random randomNumberGenerator = new Random();
+            int generatedNumber = 0;
+            //clear the list box
+            lstMarks.Items.Clear();
+            lblAverage2.Text = "";
+
+            //while counter is less than 9
+            for (int counter = 0; counter < 9; counter ++)
+            {
+                //generate a random number, add it to the array, add it to the list box, refresh the form
+                generatedNumber = randomNumberGenerator.Next(1, MAX_SIZE + 1);
+                arrayOfMarks[counter] = generatedNumber;
+                lstMarks.Items.Add(generatedNumber);
+                this.Refresh();
+                
+            }
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            double average = 0;
+            double sum = 0;
+
+            //while counter is less than 10
+            for (int counter = 0; counter < arrayOfMarks.Length; counter++)
+            {
+                //calculate the sum of the marks
+                sum += arrayOfMarks[counter];
+            }
+
+            //calculate the average and display it
+            average = sum / arrayOfMarks.Length;
+            lblAverage2.Text = Convert.ToString(average);
+
+        }
+
+        private void frmArrays_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
