59103896670f8afce4bf34ed4ef7a5d35f7e9f00
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-11-30 10:47:20 -0500

    change

---
+++ MaxValueValeriaV/MaxValueValeriaV/MaxForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace MaxValueValeriaV
+{
+    partial class frmMaxVal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstValues = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnStart.Location = new System.Drawing.Point(100, 25);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(89, 35);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstValues
+            // 
+            this.lstValues.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstValues.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.lstValues.FormattingEnabled = true;
+            this.lstValues.ItemHeight = 16;
+            this.lstValues.Location = new System.Drawing.Point(85, 77);
+            this.lstValues.Name = "lstValues";
+            this.lstValues.Size = new System.Drawing.Size(120, 180);
+            this.lstValues.TabIndex = 1;
+            // 
+            // frmMaxVal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 280);
+            this.Controls.Add(this.lstValues);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMaxVal";
+            this.Text = "Max Value by Valeria V";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstValues;
+    }
+}
+
---
+++ MaxValueValeriaV/MaxValueValeriaV/MaxForm.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Created by: Valeria Veverita
+ * Created on: 30-November-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #37 - Find Max Value
+ * This program an array of size 10 (MAX_ARRAY_SIZE), when the user clicks on the "Start" button it generates 10 random numbers, each between 1 and 500 (MAX_RANDOM_NUMBER)
+   For each random number generated it displays it in a list box and  assigns it to the subsequent cell  in the array
+   It then calls a function that:
+                                    accepts the array passed by value
+                                    scans the array to find the largest value 
+                                    returns the largest value as a single variable
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaxValueValeriaV
+{
+    public partial class frmMaxVal : Form
+    {
+        public frmMaxVal()
+        {
+            InitializeComponent();
+
+        }
+
+       
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declare local varaibles
+            Random randomNumber = new Random();
+            int chosenValue;
+            int maxValue;
+
+            //create an array of size 10
+            int[] arrayOfValues = new int[10];
+
+            //clear the list box
+            lstValues.Items.Clear();
+
+            //initialize the counter to 0, check if the counter is less than 10
+            for (int counter = 0; counter < 10; counter ++)
+            {
+                //generate a random number
+                chosenValue = randomNumber.Next(1, 501);
+                //assign it to the subsequent cell
+                arrayOfValues[counter] = chosenValue;
+                //add the item to the list box
+                lstValues.Items.Add(chosenValue);
+            }
+
+            //call the function Findmax, send the array as an argumnet, assign the return to the maxValue
+            maxValue =  FindMax(arrayOfValues);
+            //display ina message box the max value
+            MessageBox.Show("The maximum value is " + maxValue);
+
+
+        }
+        //Function: FindMax
+        //Input: int[]tmpArray
+        //Output: int tmpmaxValue
+        //Description:  this function scans the array to find the largest value  and returns the largest value as a single variable
+
+        private int FindMax(int[] tmpArray)
+        {
+            int tmpMaxValue = 0;
+            tmpMaxValue = tmpArray.Max();
+            return tmpMaxValue;
+        }
+    }
+}
